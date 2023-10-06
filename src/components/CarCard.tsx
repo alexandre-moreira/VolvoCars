3f14e1894b72cec8f3cf713b8470ff91d359820a
Alexandre Moreira <alexandremoreira@Macs-MacBook-Pro.local>
2023-10-06 18:03:27 -0300

    feat: create card component

---
+++ src/components/CarCard.tsx
@@ -0,0 +1,42 @@
+import { Card, CardContent, Text, Spacer, Flex, Row, Link } from "vcc-ui";
+import { Car } from "../types/car.interface";
+import Image from "next/image"
+import style from "../../public/css/CarCard.module.css"
+interface CardProps {
+  car: Car;
+}
+
+export function CarCard({ car }: CardProps) {
+  return (
+    <div className={style.cardWrapper}>
+        <Text variant="bates" subStyle="emphasis">
+          {car?.bodyType}
+        </Text>
+        <Flex extend={{
+          justifyContent:'flex-start',
+          alignItems: 'center',
+          flexDirection: 'row',
+          margin: 0,
+        }}>
+          
+            <Text variant="amundsen" extend={{margin:0, padding:0}}>{car?.modelName}</Text>
+            <Text variant="bates" subStyle="inline-link" extend={{margin:0}}>
+              {car?.modelType}
+            </Text>
+          
+        </Flex>
+
+        <Spacer />
+        <Image src={car.imageUrl} alt={car.modelName} width="250"height="200" />
+        <Spacer />
+        <Flex extend={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'row'
+        }}>
+          <Link href="https://www.volvocars.com/" arrow={"right"}>SHOP</Link>
+          <Link href="https://www.volvocars.com/" arrow={"right"}>LEARN</Link>
+        </Flex>
+    </div>
+  );
+}
